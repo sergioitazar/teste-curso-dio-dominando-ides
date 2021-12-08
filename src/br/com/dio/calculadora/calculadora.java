f6bdd935d16af9a3b4c74ede8bdd7a3cd96094a3
Sergio Itazar <itazar@gmail.com>
2021-12-07 22:03:40 -0300

    inclusao da calculadora

---
+++ src/br/com/dio/calculadora/calculadora.java
@@ -0,0 +1,42 @@
+package br.com.dio.calculadora;
+
+import java.util.Scanner;
+
+public class calculadora {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		 
+		int a, b;
+		
+		System.out.println("Digite o primeiro numero: "); 
+		a = scan.nextInt();
+		
+		System.out.println("Digite o segundo numero: ");
+		b = scan.nextInt();
+		
+		int soma = soma(a,b);
+		int subtracao = subtracao(a,b);
+		int multiplicacao = multiplicacao(a,b);
+		int divisao = divisao(a,b);
+		
+		System.out.println("soma " + soma);
+		System.out.println("subtração " + subtracao);
+		System.out.println("multiplicação " + multiplicacao);
+		System.out.println("divisão " + divisao);
+
+	}
+	
+	public static int soma(int a ,int b) {
+		return a + b;
+	}
+	public static int subtracao (int a ,int b) {
+		return a - b;
+	}
+	public static int divisao (int a ,int b) {
+		return a / b;
+	}
+	public static int multiplicacao (int a ,int b) {
+		return a * b;
+	}
+}
